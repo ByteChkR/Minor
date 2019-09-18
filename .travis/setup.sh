4b6fa2d5ed4adb02c06901166f509af56dce59be
Tim Akermann <ti.akermann@gmail.com>
2019-09-18 09:15:00 +0200

    no message

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -5,7 +5,16 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     # Install some custom requirements on Linux
     echo "Setting up Ubuntu OpenCL Prerequisites"
 	
-	sudo apt-get install -qq fglrx=2:8.960-0ubuntu1 opencl-headers
+	wget https://jenkins.choderalab.org/userContent/AMD-APP-SDKInstaller-v3.0.130.135-GA-linux64.tar.bz2;
+    tar -xjf AMD-APP-SDK*.tar.bz2;
+    AMDAPPSDK=${HOME}/AMDAPPSDK;
+    export OPENCL_VENDOR_PATH=${AMDAPPSDK}/etc/OpenCL/vendors;
+    mkdir -p ${OPENCL_VENDOR_PATH};
+    sh AMD-APP-SDK*.sh --tar -xf -C ${AMDAPPSDK};
+    echo libamdocl64.so > ${OPENCL_VENDOR_PATH}/amdocl64.icd;
+    export LD_LIBRARY_PATH=${AMDAPPSDK}/lib/x86_64:${LD_LIBRARY_PATH};
+    chmod +x ${AMDAPPSDK}/bin/x86_64/clinfo;
+    ${AMDAPPSDK}/bin/x86_64/clinfo;
     
     #sudo apt-get -qq update
     #sudo apt-get install pocl
