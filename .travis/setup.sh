76a1b20a1cc5392de2d73d8c6701dd20a554b293
Tim Akermann <ti.akermann@gmail.com>
2019-09-18 09:08:57 +0200

    no message

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -4,7 +4,9 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 
     # Install some custom requirements on Linux
     echo "Setting up Ubuntu OpenCL Prerequisites"
-
-    sudo apt-get -qq update
-    sudo apt-get install pocl
+	
+	sudo apt-get install -qq fglrx=2:8.960-0ubuntu1 opencl-headers
+    
+    #sudo apt-get -qq update
+    #sudo apt-get install pocl
 fi
