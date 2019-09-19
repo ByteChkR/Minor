d95e510aeaaafd2707f0dea2073656156ca63a86
Tim Akermann <ti.akermann@gmail.com>
2019-09-19 15:56:23 +0200

    Added Build Shell scripts.

---
+++ build_project.sh
@@ -0,0 +1,19 @@
+@echo off
+
+echo Building Project:
+
+echo Setting up Submodules:
+
+bash build_submodules.sh
+
+echo Setting up MinorEngine:
+
+dotnet restore MinorEngine/
+
+echo Building MinorEngine:
+
+dotnet build MinorEngine/ -c Release
+
+echo Running Tests on MinorEngine:
+
+dotnet test MinorEngine/ -p:Configuration=Release
---
+++ build_submodules.sh
@@ -0,0 +1,39 @@
+@echo off
+
+echo Setting up ADL:
+
+dotnet restore ADL/ADL/
+
+echo Setting up ext-pp:
+
+dotnet restore ext-pp/
+
+echo Setting up opencl-dotnet:
+
+dotnet restore opencl-dotnet/
+
+
+echo Building ADL Project:
+
+dotnet build ADL/ADL/ADL/ADL.csproj -c Release
+dotnet build ADL/ADL/ADL.Crash/ADL.Crash.csproj -c Release
+dotnet build ADL/ADL/ADL.Network.Client/ADL.Network.Client.csproj -c Release
+dotnet build ADL/ADL/ADL.Network.Server/ADL.Network.Server.csproj -c Release
+dotnet build ADL/ADL/ADL.Network.Shared/ADL.Network.Shared.csproj -c Release
+dotnet build ADL/ADL/ADL.UnitTests/ADL.UnitTests.csproj -c Release
+
+echo Building ext-pp Project:
+
+dotnet build ext-pp/ext_pp.sln -c Release
+
+echo Building opencl-dotnet Project:
+
+dotnet build opencl-dotnet/OpenCl.DotNetCore.sln -c Release
+
+echo Running Tests on ext-pp:
+
+dotnet test ext-pp/ext_pp.sln -p:Configuration=Release
+
+echo Running Tests on ADL:
+
+dotnet test ADL/ADL/ADL.UnitTests/ADL.UnitTests.csproj -p:Configuration=Release
