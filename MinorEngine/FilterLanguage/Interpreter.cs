7756abe605c64b550600dae0a525cd5f86f5e60c
Tim Akermann <ti.akermann@gmail.com>
2019-09-19 17:52:13 +0200

    Commented the FL Interpreter

--- MinorEngine/FilterLanguage/Interpreter.cs
+++ MinorEngine/FilterLanguage/Interpreter.cs
@@ -16,46 +16,164 @@ using Image = System.Drawing.Image;
 namespace FilterLanguage
 {
 
+    /// <summary>
+    /// The FL Interpreter
+    /// </summary>
     public class Interpreter
     {
+        /// <summary>
+        /// The key to look for when parsing defines
+        /// </summary>
         private const string DefineKey = "--define texture ";
+
+        /// <summary>
+        /// A helper variable to accomodate funky german number parsing
+        /// </summary>
         private static readonly CultureInfo NumberParsingHelper = new CultureInfo(CultureInfo.InvariantCulture.LCID);
+
+        /// <summary>
+        /// A delegate used for special functions in the interpreter
+        /// </summary>
         private delegate void FlFunction();
+
+        /// <summary>
+        /// A random that is used to provide random bytes
+        /// </summary>
         private readonly Random rnd = new Random();
-        public struct InterpreterStepResult:IEquatable<InterpreterStepResult>
+
+        /// <summary>
+        /// A struct that gets returned every step of the interpreter. Mostly for debuggin purposes and when hitting a break point
+        /// </summary>
+        public struct InterpreterStepResult : IEquatable<InterpreterStepResult>
         {
+            /// <summary>
+            /// A flag that is set when the Interpreter jumped in the program code in the last step
+            /// </summary>
             public bool HasJumped { get; set; }
+
+            /// <summary>
+            /// A flag that is set when the interpreter reached the end of the script
+            /// </summary>
             public bool Terminated { get; set; }
+
+            /// <summary>
+            /// A flag that is set when the interpreter is currently at a break statement
+            /// </summary>
             public bool TriggeredDebug { get; set; }
+
+            /// <summary>
+            /// The Currently active buffer.
+            /// </summary>
             public MemoryBuffer DebugBuffer { get; set; }
 
+            /// <summary>
+            /// IEquatable Implementation
+            /// </summary>
+            /// <param name="other">Other</param>
+            /// <returns>a Not implemented exception</returns>
             public bool Equals(InterpreterStepResult other)
             {
                 throw new NotImplementedException();
             }
         }
 
-
+        /// <summary>
+        /// A Dictionary containing the special functions of the interpreter, indexed by name
+        /// </summary>
         private readonly Dictionary<string, FlFunction> _flFunctions;
 
+        /// <summary>
+        /// The kernel database that provides the Interpreter with kernels to execute
+        /// </summary>
         private KernelDatabase _kernelDb;
 
+        /// <summary>
+        /// The Source of the script in lines
+        /// </summary>
         private List<string> _source;
+
+        /// <summary>
+        /// The current index in the program source
+        /// </summary>
         private int _currentIndex;
+
+        /// <summary>
+        /// The current word in the line at current index
+        /// </summary>
         private int _currentWord;
+
+        /// <summary>
+        /// The argument stack that is currently beeing worked on
+        /// </summary>
         private Stack<object> _currentArgStack;
+
+        /// <summary>
+        /// The active buffer
+        /// </summary>
         private MemoryBuffer _currentBuffer;
+
+        /// <summary>
+        /// The jump stack containing all the previous jumps
+        /// </summary>
         private readonly Stack<InterpreterState> _jumpStack = new Stack<InterpreterState>();
-        private int _width, _height, _depth, _channelCount;
+
+        /// <summary>
+        /// The width of the input buffer
+        /// </summary>
+        private int _width;
+
+        /// <summary>
+        /// The height of the input buffer
+        /// </summary>
+        private int _height;
+
+        /// <summary>
+        /// The depth of the input buffer
+        /// </summary>
+        private int _depth;
+
+        /// <summary>
+        /// The Channel count
+        /// </summary>
+        private int _channelCount;
+
+        /// <summary>
+        /// A property that returns the input buffer size
+        /// </summary>
         private int InputBufferSize => _width * _height * _depth * _channelCount;
+
+        /// <summary>
+        /// A byte array of the current active channels (0 = Inactive|1 = Active)
+        /// </summary>
         private byte[] _activeChannels;
+
+        /// <summary>
+        /// The memory buffer containing the active channels
+        /// The buffer gets updated as soon the channel configuration changes
+        /// </summary>
         private MemoryBuffer _activeChannelBuffer;
 
-
+        /// <summary>
+        /// The buffers indexed by name that were defined with the DefineKey
+        /// </summary>
         private readonly Dictionary<string, MemoryBuffer> _definedBuffers = new Dictionary<string, MemoryBuffer>();
+        /// <summary>
+        /// A list of possible jump locations
+        /// </summary>
         private readonly Dictionary<string, int> _jumpLocations = new Dictionary<string, int>();
+        /// <summary>
+        /// a flag that indicates if the stack should not be deleted(get used when returning from a jump)
+        /// </summary>
         private bool _leaveStack;
+
+        /// <summary>
+        /// A flag that when set to true will ignore the break statement
+        /// </summary>
         private bool _ignoreDebug;
+
+        /// <summary>
+        /// The current step result
+        /// </summary>
         private InterpreterStepResult _stepResult;
         private int EntryIndex
         {
@@ -70,12 +188,17 @@ namespace FilterLanguage
             }
         }
 
-
+        /// <summary>
+        /// A flag that indicates if the Interpreter reached the end of the script
+        /// </summary>
         public bool Terminated { get; private set; }
 
 
         #region FL_Functions
 
+        /// <summary>
+        /// The implementation of the command setactive
+        /// </summary>
         private void cmd_setactive()
         {
             if (_currentArgStack.Count < 1)
@@ -139,11 +262,18 @@ namespace FilterLanguage
 
         }
 
+        /// <summary>
+        /// A function used as RandomFunc of type byte>
+        /// </summary>
+        /// <returns>a random byte</returns>
         private byte randombytesource()
         {
             return (byte)rnd.Next();
         }
 
+        /// <summary>
+        /// The implementation of the command random
+        /// </summary>
         private void cmd_writerandom()
         {
             if (_currentArgStack.Count == 0)
@@ -161,11 +291,17 @@ namespace FilterLanguage
             }
         }
 
+        /// <summary>
+        /// The implementation of the command jmp
+        /// </summary>
         private void cmd_jump() //Dummy function. Implementation in Analyze(code) function(look for isDirectExecute)
         {
             this.Log("Jumping.", DebugChannel.Log);
         }
 
+        /// <summary>
+        /// The implementation of the command brk
+        /// </summary>
         private void cmd_break()
         {
             if (_ignoreDebug)
@@ -196,7 +332,17 @@ namespace FilterLanguage
         #endregion
 
 
-
+        /// <summary>
+        /// A public constructor
+        /// </summary>
+        /// <param name="file">The file containing the source</param>
+        /// <param name="input">The input buffer</param>
+        /// <param name="width">Width of the input buffer</param>
+        /// <param name="height">Height of the input buffer</param>
+        /// <param name="depth">Depth of the input buffer</param>
+        /// <param name="channelCount">The Channel Count</param>
+        /// <param name="kernelDB">The Kernel DB that will be used</param>
+        /// <param name="ignoreDebug">a flag to ignore the brk statement</param>
         public Interpreter(string file, MemoryBuffer input, int width, int height, int depth, int channelCount, KernelDatabase kernelDB,
             bool ignoreDebug)
         {
@@ -214,25 +360,78 @@ namespace FilterLanguage
 
             Reset(file, input, width, height, depth, channelCount, kernelDB, ignoreDebug);
         }
+
+        /// <summary>
+        /// A public constructor
+        /// </summary>
+        /// <param name="file">The file containing the source</param>
+        /// <param name="input">The input buffer</param>
+        /// <param name="width">Width of the input buffer</param>
+        /// <param name="height">Height of the input buffer</param>
+        /// <param name="depth">Depth of the input buffer</param>
+        /// <param name="channelCount">The Channel Count</param>
+        /// <param name="ignoreDebug">a flag to ignore the brk statement</param>
         public Interpreter(string file, MemoryBuffer input, int width, int height, int depth, int channelCount, string kernelDBFolder,
             bool ignoreDebug) : this(file, input, width, height, depth, channelCount, new KernelDatabase(kernelDBFolder), ignoreDebug) { }
+
+        /// <summary>
+        /// A public constructor
+        /// </summary>
+        /// <param name="file">The file containing the source</param>
+        /// <param name="input">The input buffer</param>
+        /// <param name="width">Width of the input buffer</param>
+        /// <param name="height">Height of the input buffer</param>
+        /// <param name="depth">Depth of the input buffer</param>
+        /// <param name="channelCount">The Channel Count</param>
         public Interpreter(string file, MemoryBuffer input, int width, int height, int depth, int channelCount, string kernelDBFolder) : this(file, input, width, height, depth, channelCount, new KernelDatabase(kernelDBFolder), false) { }
+        
+        /// <summary>
+        /// A public constructor
+        /// </summary>
+        /// <param name="file">The file containing the source</param>
+        /// <param name="input">The input buffer</param>
+        /// <param name="width">Width of the input buffer</param>
+        /// <param name="height">Height of the input buffer</param>
+        /// <param name="depth">Depth of the input buffer</param>
+        /// <param name="channelCount">The Channel Count</param>
+        /// <param name="kernelDB">The Kernel DB that will be used</param>
         public Interpreter(string file, MemoryBuffer input, int width, int height, int depth, int channelCount, KernelDatabase kernelDB) : this(file, input, width, height, depth, channelCount, kernelDB, false) { }
 
-        public void Reset(string file, MemoryBuffer input, int width, int height, int depth, int chanelCount,
+        /// <summary>
+        /// Resets the Interpreter to work with a new script
+        /// </summary>
+        /// <param name="file">The file containing the source</param>
+        /// <param name="input">The input buffer</param>
+        /// <param name="width">Width of the input buffer</param>
+        /// <param name="height">Height of the input buffer</param>
+        /// <param name="depth">Depth of the input buffer</param>
+        /// <param name="channelCount">The Channel Count</param>
+        /// <param name="kernelDB">The Kernel DB that will be used</param>
+        public void Reset(string file, MemoryBuffer input, int width, int height, int depth, int channelCount,
             KernelDatabase kernelDB)
         {
-            Reset(file, input, width, height, depth, chanelCount, kernelDB, false);
+            Reset(file, input, width, height, depth, channelCount, kernelDB, false);
         }
 
-        public void Reset(string file, MemoryBuffer input, int width, int height, int depth, int chanelCount, KernelDatabase kernelDB, bool ignoreDebug)
+        /// <summary>
+        /// Resets the Interpreter to work with a new script
+        /// </summary>
+        /// <param name="file">The file containing the source</param>
+        /// <param name="input">The input buffer</param>
+        /// <param name="width">Width of the input buffer</param>
+        /// <param name="height">Height of the input buffer</param>
+        /// <param name="depth">Depth of the input buffer</param>
+        /// <param name="channelCount">The Channel Count</param>
+        /// <param name="kernelDB">The Kernel DB that will be used</param>
+        /// <param name="ignoreDebug">a flag to ignore the brk statement</param>
+        public void Reset(string file, MemoryBuffer input, int width, int height, int depth, int channelCount, KernelDatabase kernelDB, bool ignoreDebug)
         {
             _currentBuffer = input;
             this._ignoreDebug = ignoreDebug;
             this._width = width;
             this._height = height;
             this._depth = depth;
-            this._channelCount = chanelCount;
+            this._channelCount = channelCount;
             this._kernelDb = kernelDB;
             _activeChannels = new byte[_channelCount];
             for (int i = 0; i < _channelCount; i++)
@@ -264,17 +463,28 @@ namespace FilterLanguage
         }
 
 
-
+        /// <summary>
+        /// Returns the currently active buffer
+        /// </summary>
+        /// <returns>The active buffer</returns>
         public MemoryBuffer GetResultBuffer()
         {
             return _currentBuffer;
         }
 
+        /// <summary>
+        /// Returns the currently active buffer
+        /// </summary>
+        /// <returns>The active buffer read from the gpu and placed in cpu memory</returns>
         public T[] GetResult<T>() where T : struct
         {
             return CL.ReadBuffer<T>(_currentBuffer, (int)_currentBuffer.Size);
         }
 
+        /// <summary>
+        /// "Simulates a step on a processor
+        /// </summary>
+        /// <returns>The Information about the current step(mostly for debugging)</returns>
         public InterpreterStepResult Step()
         {
 
@@ -310,6 +520,9 @@ namespace FilterLanguage
         }
 
 
+        /// <summary>
+        /// Finds all jump locations inside the script
+        /// </summary>
         private void ParseJumpLocations()
         {
             for (int i = _source.Count - 1; i >= 0; i--)
@@ -323,6 +536,11 @@ namespace FilterLanguage
         }
 
 
+        /// <summary>
+        /// Analyzes a line of code
+        /// </summary>
+        /// <param name="code">the line to analyze</param>
+        /// <returns>True if the program counter should be increased</returns>
         bool Analyze(string code)
         {
 
@@ -391,13 +609,19 @@ namespace FilterLanguage
 
                     this.Log("Running kernel: " + function, DebugChannel.Log);
                     CL.Run(kernel, _currentBuffer, new int3(_width, _height, _depth), _activeChannelBuffer, _channelCount); //Running the kernel
-                    
+
                 }
 
             }
             return ret;
         }
 
+        /// <summary>
+        /// Analyzes a single word of a line
+        /// </summary>
+        /// <param name="word">The input word</param>
+        /// <param name="val">the parsed output</param>
+        /// <returns>if the word could be parsed</returns>
         bool AnalyzeWord(string word, out object val)
         {
             if (_jumpLocations.ContainsKey(word))
@@ -431,16 +655,29 @@ namespace FilterLanguage
             }
         }
 
+        /// <summary>
+        /// Resets the Interpreter program counter and word counter to the beginning
+        /// </summary>
         private void Reset()
         {
             _currentIndex = EntryIndex;
             _currentWord = 1;
         }
+
+        /// <summary>
+        /// Returns true if the text is surrounded by the surrStr
+        /// </summary>
+        /// <param name="text">The text to be checked</param>
+        /// <param name="surrStr">the pattern to search for</param>
+        /// <returns>True of the text is surrounded by the surrStr</returns>
         private static bool IsSurroundedBy(string text, string surrStr)
         {
             return text.StartsWith(surrStr) && text.EndsWith(surrStr);
         }
 
+        /// <summary>
+        /// Finds, Parses and Loads all define statements
+        /// </summary>
         private void ParseDefines()
         {
             for (int i = _source.Count - 1; i >= 0; i--)
@@ -489,7 +726,7 @@ namespace FilterLanguage
 #if NO_CL
                         Bitmap bmp = null;
 #else
-                        Bitmap bmp = (Bitmap) Image.FromFile(filename.Replace("\"", ""));
+                        Bitmap bmp = (Bitmap)Image.FromFile(filename.Replace("\"", ""));
 #endif
                         _definedBuffers.Add(varname,
                             CL.CreateFromImage(bmp,
@@ -561,6 +798,11 @@ namespace FilterLanguage
                 }
             }
         }
+
+
+        /// <summary>
+        /// Detects if the Interpreter has reached the end of the current function
+        /// </summary>
         void DetectEnd()
         {
             if (_currentIndex == _source.Count || _source[_currentIndex].EndsWith(":"))
@@ -593,9 +835,14 @@ namespace FilterLanguage
                     _currentWord = lastState.ArgumentStack.Count + 1;
                 }
             }
-            
+
         }
 
+        /// <summary>
+        /// Jumps the interpreter to the specified index
+        /// </summary>
+        /// <param name="index">the index of the line to jump to</param>
+        /// <param name="keepBuffer">a flag to optionally keep the current buffer</param>
         void JumpTo(int index, bool keepBuffer = false)
         {
             _jumpStack.Push(new InterpreterState(_currentIndex, _currentBuffer, _currentArgStack));
@@ -603,12 +850,16 @@ namespace FilterLanguage
             int size = (int)_currentBuffer.Size;
             if (!keepBuffer)
             {
-                _currentBuffer = CL.CreateEmpty<byte>(size, MemoryFlag.ReadWrite|MemoryFlag.CopyHostPointer);
+                _currentBuffer = CL.CreateEmpty<byte>(size, MemoryFlag.ReadWrite | MemoryFlag.CopyHostPointer);
             }
             _currentIndex = index;
             _currentWord = 1;
         }
 
+        /// <summary>
+        /// Loads the source from file
+        /// </summary>
+        /// <param name="file"></param>
         void LoadSource(string file)
         {
 
