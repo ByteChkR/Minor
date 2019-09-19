13ef5848346431b356bba03229cf378b71f91904
Tim Akermann <ti.akermann@gmail.com>
2019-09-19 16:09:29 +0200

    Fixed Linux build.

--- build_project.sh
+++ build_project.sh
@@ -17,4 +17,4 @@ dotnet build MinorEngine/ -c Release
 
 echo Running Tests on MinorEngine:
 
-dotnet test MinorEngine/ -p:Configuration=Release
+dotnet test MinorEngine/ -p:Configuration=Release -p:DefineConstants=NO_CL
