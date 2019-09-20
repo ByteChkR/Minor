4f12d106a18bb8c1e28c788dc1f7b0745c09c943
Tim Akermann <ti.akermann@gmail.com>
2019-09-20 09:45:33 +0200

    Removed pause statement from batch file.

--- start_engine.bat
+++ start_engine.bat
@@ -6,4 +6,3 @@ IF NOT EXIST "MinorEngine\MinorEngine\bin\Release\netcoreapp2.1\MinorEngine.dll"
 echo Running Engine...
 cd MinorEngine\MinorEngine\assets
 dotnet run --launch-profile "GameEngine" -p "..\MinorEngine.csproj"
-pause
