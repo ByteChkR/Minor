1467c0b1cddd2adec92c4feddab4de5600a51516
Tim Akermann <ti.akermann@gmail.com>
2019-10-02 23:57:23 +0200

    Fixed the look at function(have to investigate why the matrix needs to be constructed so wierdly. Added ADL.Network.Shared/Client to the Common project. It will ask for permission to upload the console logs. Improved Performance of the FL Generator Fixed a bug related to updating GL Textures from byte arrays Removed the Demo Scene that has been unused for a while. Updated submodule bepuphysics2 and ADL to newest commit

---
+++ MinorEngine/LogServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogServer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            new ADL.Network.Server.ServerConsole();
+        }
+    }
+}
