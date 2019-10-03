69b35955fa66353225c580fa0cf5d268ab8697c3
Tim Akermann <ti.akermann@gmail.com>
2019-10-03 17:06:11 +0200

    Fixed folder naming.

---
+++ MinorEngine/FilterLanguage/debug_helper/DebugChannel.cs
@@ -0,0 +1,16 @@
+﻿namespace FilterLanguage.debug
+{
+    /// <summary>
+    /// An enum of Channels that can be used to send logs
+    /// </summary>
+    internal enum DebugChannel
+    {
+        ALL = -1,
+        None = 0,
+        Error = 1,
+        Warning = 2,
+        Log = 4,
+        Internal_Error = 8,
+        Progress = 16
+    }
+}
---
+++ MinorEngine/MinorEngine/debug_helper/DebugChannel.cs
@@ -0,0 +1,16 @@
+﻿namespace MinorEngine.debug
+{
+    /// <summary>
+    /// An enum of Channels that can be used to send logs
+    /// </summary>
+    public enum DebugChannel
+    {
+        ALL = -1,
+        None = 0,
+        Error = 1,
+        Warning = 2,
+        Log = 4,
+        Internal_Error = 8,
+        Progress = 16
+    }
+}
