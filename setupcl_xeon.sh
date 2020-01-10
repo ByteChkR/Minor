eeadb0dbcc5936b79555da4399c8982be23397e3
Tim Akermann <ti.akermann@gmail.com>
2020-01-10 10:10:29 +0100

    Trying Specific Xeon OpenCL Drivers for travis build.

---
+++ setupcl_xeon.sh
@@ -0,0 +1,7 @@
+    # download and unpack base opencl Intel drivers
+    wget http://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/15532/l_opencl_p_18.1.0.015.tgz
+    tar -xvf l_opencl_p_18.1.0.015.tgz
+    chmod +x l_opencl_p_18.1.0.015/*.sh
+    cs l_opencl_p_18.1.0.015
+    bash install.sh
+    cd ..
