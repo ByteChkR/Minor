e55acd8c7f6f1042b47bf3a46117a539a4e84b8a
Tim Akermann <ti.akermann@gmail.com>
2020-01-10 10:25:05 +0100

    Fixed Typo

--- setupcl_xeon.sh
+++ setupcl_xeon.sh
@@ -2,6 +2,6 @@
     wget http://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/15532/l_opencl_p_18.1.0.015.tgz
     tar -xvf l_opencl_p_18.1.0.015.tgz
     chmod +x l_opencl_p_18.1.0.015/*.sh
-    cs l_opencl_p_18.1.0.015
+    cd l_opencl_p_18.1.0.015
     bash install.sh
     cd ..
